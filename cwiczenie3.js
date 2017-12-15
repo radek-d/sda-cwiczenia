1d1e6a9065e7a139a11a49cd317047a07214fdaa
Szymon Rybka <szymon.rybka@jeppesen.com>
2017-12-15 20:11:17 +0100

    added new solution

--- cwiczenie3.js
+++ cwiczenie3.js
@@ -2,10 +2,27 @@
  * Napisz metode ktora posortuje wyniki, na podstawie przeslanej nazwy pola
  */
 
-var library = [ 
-    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
-    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
-    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
-    ];
+var library = [
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254 },
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264 },
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245 }
+];
 
-    
+function sortuj(jakiePole) {
+    library.sort((a, b) => {
+        if (a[jakiePole] > b[jakiePole]) {
+            return 1;
+        } else if (a[jakiePole] < b[jakiePole]) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+}
+
+
+sortuj("title");
+
+
+
+console.log(library);
