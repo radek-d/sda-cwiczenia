2ba2c67021c38b9ddb3a6bfd263a88b13b889110
rdab <radek.db@gmail.com>
2017-12-15 20:30:00 +0100

    Ex3

--- cwiczenie3.js
+++ cwiczenie3.js
@@ -7,3 +7,51 @@ var library = [
     { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
     { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
     ];
+
+function sortBy(argument) {
+    var library_sorted = [];
+        library_sorted = library.forEach(sort(howToSort(argument)))
+    return library_sorted;
+}
+
+function howToSort(tem1, item2) {
+    if ((item1.author) > (item2.author)) {
+        return 1;
+    } 
+    else if ((item2.author) > (item1.author))
+    {
+        return -1;
+    }
+    else 
+    {
+        return 0;
+    }
+
+    if ((item1.title) > (item2.title)) {
+        return 1;
+    } 
+    else if ((item2.title) > (item1.title))
+    {
+        return -1;
+    }
+    else 
+    {
+        return 0;
+    }
+
+    if ((item1.libraryID) > (item2.libraryID)) {
+        return 1;
+    } 
+    else if ((item2.libraryID) > (item1.libraryID))
+    {
+        return -1;
+    }
+    else 
+    {
+        return 0;
+    }
+
+
+}
+
+console.log("Sortuj po Autorze:", library.sortBy(author));
