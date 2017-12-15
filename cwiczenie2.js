2bf057cb6bb4a729b49b10e8915896b2dc6d0035
Szymon Rybka <szymon.rybka@jeppesen.com>
2017-12-15 18:53:58 +0100

    added solution 2

--- cwiczenie2.js
+++ cwiczenie2.js
@@ -11,5 +11,36 @@
  * exe, bat              - aplikacje
  */
 
-var files = ["obrazek.jpg", "avatar.png", "cv.pdf", "finanse.xls"];
-var types = ...
+var files = ["obrazek.jpg", "avatar.png", "cv.pdf", "finanse.xls", "a.jpg.exe"];
+// var types = 
+files.forEach( element => {
+    showType(element);
+ });
+
+function showType(fileName) {
+    var result;
+    var type = getType(fileName);
+    switch (type) {
+        case "jpg":
+        case "png":
+        case "psd":
+            result = "obraz";
+            break
+        case "doc":
+        case "txt":
+        case "xls":
+        case "pdf":
+            result = "dokument"
+            break
+        case "exe":
+        case "bat":
+            result = "aplikacja"
+            break
+    }
+    console.log (fileName, "jest typu", result);
+
+    function getType(fileName) {
+        var array = fileName.split(".");
+        return array[array.length - 1];
+    }
+}
