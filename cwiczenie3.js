3df1d0ebb0e6e287e9dfc419aff7b473d2fe7359
Szymon Rybka <szymon.rybka@jeppesen.com>
2017-12-15 18:57:59 +0100

    added new excercise

---
+++ cwiczenie3.js
@@ -0,0 +1,9 @@
+/*
+ * Napisz metode ktora posortuje wyniki, na podstawie przeslanej nazwy pola
+ */
+
+var library = [ 
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+    ];
